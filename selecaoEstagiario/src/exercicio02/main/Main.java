464fe5b7f91e0fabd53fe24a1b5873e379ff4254
José Gomes <106168901+josegomes00@users.noreply.github.com>
2023-03-31 13:33:48 -0300

    Update Main.java

--- selecaoEstagiario/src/exercicio02/main/Main.java
+++ selecaoEstagiario/src/exercicio02/main/Main.java
@@ -5,6 +5,8 @@ import exercicio02.media.TotalMedia;
 public class Main {
     public static void main(String[] args) {
         TotalMedia t = new TotalMedia();
+        System.out.println(t.getMedia789());
+        System.out.println(t.getMedia456());
         System.out.println(t.SomaMedias());
         System.out.println(t.mediaMedias());
     }
