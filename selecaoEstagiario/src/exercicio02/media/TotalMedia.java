a046b7f04bd299e2dbddbc5eb4353df8393c14b9
José Gomes <106168901+josegomes00@users.noreply.github.com>
2023-03-31 13:33:32 -0300

    Update TotalMedia.java

--- selecaoEstagiario/src/exercicio02/media/TotalMedia.java
+++ selecaoEstagiario/src/exercicio02/media/TotalMedia.java
@@ -1,8 +1,27 @@
 package exercicio02.media;
 
 public class TotalMedia {
-    int media789 = (7+8+9)/3;
-    int media456 = (4+5+6)/3;
+    private int media789 = (7+8+9)/3;
+    private int media456 = (4+5+6)/3;
+
+    public TotalMedia() {
+    }
+
+    public int getMedia789() {
+        return media789;
+    }
+
+    public void setMedia789(int media789) {
+        this.media789 = media789;
+    }
+
+    public int getMedia456() {
+        return media456;
+    }
+
+    public void setMedia456(int media456) {
+        this.media456 = media456;
+    }
 
     public int SomaMedias(){
         int somaMedias = media456 + media789;
@@ -14,4 +33,6 @@ public class TotalMedia {
         mediaMedias = mediaMedias/2;
         return mediaMedias;
     }
+    
+    
 }
